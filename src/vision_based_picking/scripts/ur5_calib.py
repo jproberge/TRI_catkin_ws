25802d8799c1c2b57a3e6ca23ba07868f3590ae9
Michael Lin <michaelv03@gmail.com>
2019-10-31 01:29:04 -0700

    got ur5 interface abstraction working

--- src/vision_based_picking/scripts/ur5_calib.py
+++ src/vision_based_picking/scripts/ur5_calib.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+
+""" ur5_calib.py
+    script used to moving the robot to calibrate wrt to realsense camera
+    author: Michael Anres Lin (michaelv03@gmail.com)
+    date: 10/31/2019
+"""
+
 import sys
 import time
 import roslib; roslib.load_manifest('ur_driver'); roslib.load_manifest('robotiq_3f_gripper_control')
 import rospy
+from std_msgs.msg import Empty
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
@@ -32,18 +40,20 @@ class UR5Interface:
                                                         moveit_msgs.msg.DisplayTrajectory,
                                                         queue_size=20)
 
+        self.goal_state_publisher = rospy.Publisher('/rviz/moveit/update_custom_goal_state',
+                                                        moveit_msgs.msg.RobotState,
+                                                        queue_size=20)
+
         # hard coded home position
         self.pose_home = geometry_msgs.msg.Pose()
         self.pose_home.orientation.x = 0.0
         self.pose_home.orientation.y = 0.7071055
         self.pose_home.orientation.z = 0.0
         self.pose_home.orientation.w = 0.7071055
-        self.pose_home.position.x = 0.15
-        self.pose_home.position.y = 0.25
+        self.pose_home.position.x = -0.15
+        self.pose_home.position.y = 0.35
         self.pose_home.position.z = 0.25
 
-        ## BEGIN_SUB_TUTORIAL basic_info
-        ##
         ## Getting Basic Information
         ## ^^^^^^^^^^^^^^^^^^^^^^^^^
         # We can get the name of the reference frame for this robot:
@@ -63,12 +73,11 @@ class UR5Interface:
         print "============ Printing robot state"
         print self.robot.get_current_state()
         print ""
-        ## END_SUB_TUTORIAL
 
     def check_joint_limits(self):
         """ function to check that the urdf loaded is specifying
             smaller joint limits (-pi, pi) so that the planner works better """
-        for j in joint_names:
+        for j in self.joint_names:
             b = self.robot.get_joint(j).bounds()
             # If any joint has limits greater than pi then is bad bounds
             if (b[0] < -(np.pi+0.1)) or (b[1] > (np.pi+0.1)):
@@ -81,38 +90,38 @@ class UR5Interface:
         return self.group.get_current_pose().pose
 
     def goto_home_pose(self):
-        """ go to robot end effector pose target """
-        self.group.set_pose_target(self.pose_home)
+        """ go to robot end effector home pose """
+        self.goto_pose_target(self.pose_home)
 
     def goto_pose_target(self, pose):
         """ go to robot end effector pose target """
         self.group.set_pose_target(pose)
         # simulate in rviz then ask user for feedback
+        plan = self.group.plan()
+        self.display_trajectory(plan)
+        print("============ Press `Enter` to execute the movement ...")
+        raw_input()
+        self.group.execute(plan, wait=True)
+        self.group.stop()
+        self.group.clear_pose_targets()
 
 
     def display_trajectory(self, plan):
+        """ displays planned trajectory in rviz """
         # Copy class variables to local variables to make the web tutorials more clear.
         # In practice, you should use the class variables directly unless you have a good
         # reason not to.
         robot = self.robot
         display_trajectory_publisher = self.display_trajectory_publisher
 
-        ## BEGIN_SUB_TUTORIAL display_trajectory
-        ##
-        ## Displaying a Trajectory
-        ## ^^^^^^^^^^^^^^^^^^^^^^^
-        ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
-        ## group.plan() method does this automatically so this is not that useful
-        ## here (it just displays the same trajectory again):
-        ##
-        ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
-        ## We populate the trajectory_start with our current robot state to copy over
-        ## any AttachedCollisionObjects and add our plan to the trajectory.
         display_trajectory = moveit_msgs.msg.DisplayTrajectory()
         display_trajectory.trajectory_start = robot.get_current_state()
         display_trajectory.trajectory.append(plan)
-        # Publish
-        display_trajectory_publisher.publish(display_trajectory);
+        display_trajectory_publisher.publish(display_trajectory)
+        # not working for goal state yet
+        robot_goal_state = moveit_msgs.msg.RobotState()
+        robot_goal_state.joint_state.position = plan.joint_trajectory.points[-1].positions
+        self.goal_state_publisher.publish(robot_goal_state)
 
             
 
@@ -134,33 +143,28 @@ def test_move():
         ur5 = UR5Interface()
 
         # MoveIt! works well if joint limits are smaller (within -pi, pi)
-        if not check_joint_limits(robot):
+        if not ur5.check_joint_limits():
             raise Exception('Bad joint limits! try running roslaunch with option "limited:=true"')
 
 
-        current_pose = group.get_current_pose().pose
+        current_pose = ur5.get_pose()
         print "============ Current pose: %s" % current_pose
-        pose_goal = geometry_msgs.msg.Pose()
+        
+        ur5.goto_home_pose()
+
+        # go to P2
+        P2_pose = ur5.get_pose()
+        P2_pose.position.x += 0.1
+        ur5.goto_pose_target(P2_pose)
+
+        # go to P3
+        P3_pose = ur5.get_pose()
+        P3_pose.position.z += 0.1
+        ur5.goto_pose_target(P3_pose)
+
+
 
         ### sampling P1
-        pose_goal.orientation.x = 0.0
-        pose_goal.orientation.y = 0.7071055
-        pose_goal.orientation.z = 0.0
-        pose_goal.orientation.w = 0.7071055
-        pose_goal.position.x = 0.0
-        pose_goal.position.y = 0.4012076
-        pose_goal.position.z = 0.3159719
-        group.set_pose_target(pose_goal)
-
-        plan = group.go(wait=True)
-
-        group.stop()
-
-        group.clear_pose_targets()
-
-        current_pose = group.get_current_pose().pose
-        print "============ P1 pose: %s" % current_pose
-
         #resp = camera_service('Start', 1)
         #time.sleep(INTER_COMMAND_DELAY)
 
@@ -172,19 +176,6 @@ def test_move():
     #   P1 = np.array([[resp.x, resp.y, resp.z]])
 
     #   ### sampling P2
-    #   P2_pose = group.get_current_pose().pose
-    #   P2_pose.position.x += 0.1
-    #   group.set_pose_target(P2_pose)
-
-    #   plan = group.go(wait=True)
-
-    #   group.stop()
-
-    #   group.clear_pose_targets()
-
-    #   current_pose = group.get_current_pose().pose
-    #   print "============ P2 pose: %s" % current_pose
-
     #   #camera_service('Start', 1)
     #   #time.sleep(INTER_COMMAND_DELAY)
 
@@ -195,19 +186,6 @@ def test_move():
     #   P2 = np.array([[resp.x, resp.y, resp.z]])
 
     #   ### sampling P3
-    #   P3_pose = group.get_current_pose().pose
-    #   P3_pose.position.z += 0.1
-    #   group.set_pose_target(P3_pose)
-
-    #   plan = group.go(wait=True)
-
-    #   group.stop()
-
-    #   group.clear_pose_targets()
-
-    #   current_pose = group.get_current_pose().pose
-    #   print "============ P3 pose: %s" % current_pose
-
     #   #camera_service('Start', 1)
     #   #time.sleep(INTER_COMMAND_DELAY)
 
@@ -216,8 +194,6 @@ def test_move():
 
     #   #camera_service('Shutdown', 1)
     #   P3 = np.array([[resp.x, resp.y, resp.z]])
-
-        print(check_joint_limits(robot))
 
         #current_pose = group.get_current_pose().pose
         #print "============ Current pose: %s" % current_pose
